45444a58e6e8523fcde54b7faa6ae0f8907023a9
howl-lab@github.com <howl-lab@github.com>
2022-06-22 23:08:32 -0700

    add new lines to js files, created json packages

---
+++ index.js
@@ -0,0 +1,84 @@
+// TODO: Include packages needed for this application
+const js = require('js');
+const generateMarkdown = require('./utils/generateMarkdown')
+const inquirer = require('inquirer');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        message: "What's your project title?",
+        name: 'title',
+        type: 'input',
+        default: 'Project Untitled'
+    },
+    {
+        message: "What's your project description?",
+        name: 'description',
+        type: 'input',
+        default: 'About my project goes here'
+    },
+    {
+        message: "What're your installation instructions?",
+        name: 'installation',
+        type: 'input',
+        default: 'How to install here'
+    },
+    {
+        message: "What's your usage information",
+        name: 'usage',
+        type: 'input',
+        default: 'Usage info here'
+    },
+    {
+        message: "What're your contribution guidelines",
+        name: 'contribution',
+        type: 'input',
+        default: 'contribution guideline here'
+    },
+    {
+        message: "What're your test instructions",
+        name: 'test',
+        type: 'input',
+        default: 'test instructions here'
+    },
+    {
+        message: "Choose your badge license",
+        name: 'badge',
+        type: 'checkbox',
+        choices: [
+            'MIT',
+            'Apache',
+        ],
+        default: 'badge unknown'
+    },
+    {
+        message: "What is your GitHub username",
+        name: 'GitHub',
+        type: 'input',
+        default: 'GitHub name N/A'
+    },
+    {
+        message: "What is your email address",
+        name: 'email',
+        type: 'input',
+        default: 'no email'
+    },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(`${data.name}.json`, JSON.stringify(answers, null, 2), err => {
+        if (err) {
+            console.log(err);
+        }
+        console.log('done');
+    });
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(writeToFile);
+}
+
+// Function call to initialize app
+init();
